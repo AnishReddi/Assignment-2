4046332504df4c8ad9c269cd5003d188977f2031
Anish Reddy Modulla <Amodulla5571@conestogac.on.ca>
2024-02-14 19:37:57 -0500

    Add project files.

---
+++ Assignment-2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GemCollectionGame
+{
+    class Program
+    {
+        char[,] board = new char[6, 6];
+        int[,]  gems = new int[6, 6];
+        int[,]  obstacles = new int[6, 6];
+        int[] player1position = new int
+        int[] player2position = new int
+        int player1gems = 0;
+        int player2gems = 0;
+        int totalgems = 5;
+        static int maxMoves = 15;
+
+        static void Main(string[] args)
+
+    }
+    
+        
+
+    
+
+
+}
