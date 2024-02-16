73c993256f33925b838a3bd7ef4b5170d6cf588b
Anish Reddy Modulla <Amodulla5571@conestogac.on.ca>
2024-02-16 14:49:16 -0500

    1

--- Assignment-2/Program.cs
+++ Assignment-2/Program.cs
@@ -4,18 +4,65 @@ namespace GemCollectionGame
 {
     class Program
     {
-        char[,] board = new char[6, 6];
-        int[,]  gems = new int[6, 6];
-        int[,]  obstacles = new int[6, 6];
-        int[] player1position = new int
-        int[] player2position = new int
-        int player1gems = 0;
-        int player2gems = 0;
-        int totalgems = 5;
+        static char[,] board = new char[6, 6];
+        static int[,]  gems = new int[6, 6];
+        static int[,]  obstacles = new int[6, 6];
+        static int[] player1position = new int[2];
+        static int[] player2position = new int[2];
+        static int player1gems = 0;
+        static int player2gems = 0;
+        static int totalgems = 5;
         static int maxMoves = 15;
 
         static void Main(string[] args)
+        {
+            InitializeBoard();
+            PlaceGems();
+        }
 
+        static void InitializeBoard()
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                for(int j = 0; j < 6; j++)
+                {
+                    board[i, j] = '-';
+                }
+            }
+        }
+
+        static void PlaceGems()
+        {
+            Random rnd = new Random();
+            int gemsPlaced = 0;
+            while (gemsPlaced < totalgems)
+            {
+                int x = rnd.Next(0, 6);
+                int y = rnd.Next(0, 6);
+                if (gems[x, y] == 0)
+                {
+                    gems[x, y] = 1;
+                    board[x, y] = 'G';
+                    gemsPlaced++;
+                }
+            }
+        }
+
+
+        static void PlaceObstacles()
+        {
+            Random rnd = new Random();
+            int obstaclesPlaced = 0;
+            while (obstaclesPlaced < maxObstacles)
+            {
+                int x= rnd.Next(0, 6);
+                int y = rnd.Next(0, 6); 
+                if(x !=0 || y !=0)
+                {
+                    if 
+                }
+            }
+        }
     }
     
         
