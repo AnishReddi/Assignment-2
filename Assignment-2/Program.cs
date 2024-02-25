c6b1c4a967add2ea2efb66108274ab227c24d1a6
Anish Reddy Modulla <Amodulla5571@conestogac.on.ca>
2024-02-25 18:42:01 -0500

    4

--- Assignment-2/Program.cs
+++ Assignment-2/Program.cs
@@ -45,3 +45,61 @@ public class Player
         }
     }
 }
+public class Cell // represents what occupies the cell on the board
+{
+    public string Occupant { get; set; }
+
+    public Cell(string occupant = "-") // Constructor of cell class
+    {
+        Occupant = occupant;
+    }
+}
+public class Board
+{
+    public Cell[,] Grid { get; }
+
+    public Board()               // Constructor of board class
+    {
+        Grid = new Cell[6, 6];
+        for (int i = 0; i < 6; i++)
+        {
+            for (int j = 0; j < 6; j++)
+            {
+                Grid[i, j] = new Cell();
+            }
+        }
+    }
+    public void Display(Player player1, Player player2)
+    {
+        for (int i = 0; i < 6; i++)
+        {
+            for (int j = 0; j < 6; j++)
+            {
+                if (player1.Position.X == j && player1.Position.Y == i) // Checks if the current cell matches with P1 position
+                {
+                    Console.Write("P1 ");
+                }
+                else if (player2.Position.X == j && player2.Position.Y == i) //  Checks if the current cell matches with P2 position
+                {
+                    Console.Write("P2 ");
+                }
+                else
+                {
+                    string occupant = Grid[i, j].Occupant;  // If a cll doesnt contain a player, it checks the occupant
+                    if (occupant == "G")                    // Displays "G" if a cell cointains gem
+                    {
+                        Console.Write("G ");
+                    }
+                    else if (occupant == "O")               // Displays "O" if a cell cointains gem
+                    {
+                        Console.Write("O ");
+                    }
+                    else
+                    {
+                        Console.Write("- ");               // Displays "-" if a cell is empty
+                    }
+                }
+            }
+            Console.WriteLine();                           // Move to next line after displaying each row 
+        }
+    }
